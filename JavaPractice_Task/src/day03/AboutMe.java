ed8fd32b1571f191d9fdd4bb71753abe8766a79c
Nesho RADEV <40929562+RedPRO16@users.noreply.github.com>
2023-02-28 23:35:38 +0300

    task

--- JavaPractice_Task/src/day03/AboutMe.java
+++ JavaPractice_Task/src/day03/AboutMe.java
@@ -2,7 +2,7 @@ package day03;
 
 public class AboutMe {
     public static void main(String[] args) {
-        System.out.println("Favorite music: classic");
+        System.out.println("Favorite music: \"classic\",\"jazz\"".);
         System.out.println("Favorite book: Atomic habit");
         System.out.println("Favorite quote: Time is value");
         System.out.println("Favorite TV show: Dragons's Den");
