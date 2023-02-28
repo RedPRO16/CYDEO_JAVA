fe1773c8b7b3e15976a425c13a15140088cf6779
Nesho RADEV <40929562+RedPRO16@users.noreply.github.com>
2023-02-28 16:24:31 +0300

    task day03

---
+++ JavaPractice_Task/src/day03/AboutMe.java
@@ -0,0 +1,10 @@
+package day03;
+
+public class AboutMe {
+    public static void main(String[] args) {
+        System.out.println("Favorite music: classic");
+        System.out.println("Favorite book: Atomic habit");
+        System.out.println("Favorite quote: Time is value");
+        System.out.println("Favorite TV show: Dragons's Den");
+    }
+}
---
+++ JavaPractice_Task/src/day03/Diamond2.java
@@ -0,0 +1,17 @@
+package day03;
+
+public class Diamond2 {
+    public static void main(String[] args) {
+        System.out.println("     ^");
+        System.out.println("    / \\");
+        System.out.println("   / * \\ ");
+        System.out.println("  / * * \\ ");
+        System.out.println(" / * * * \\ ");
+        System.out.println(" ---------");
+        System.out.println(" \\ * * * / ");
+        System.out.println("  \\ * * / ");
+        System.out.println("   \\ * / ");
+        System.out.println("    \\ / ");
+        System.out.println("     v ");
+    }
+}
