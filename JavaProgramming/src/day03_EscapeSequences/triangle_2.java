9dd9697f73733ba97d05257a0f43722211bae185
Nesho RADEV <40929562+RedPRO16@users.noreply.github.com>
2023-03-06 14:03:00 +0000

    latest update

---
+++ JavaProgramming/src/day03_EscapeSequences/triangle_2.java
@@ -0,0 +1,20 @@
+package day03_EscapeSequences;
+
+public class triangle_2 {
+    public static void main(String[] args) {
+
+        //Escape sequence
+        // \
+        System.out.println(/n);
+    }
+}
+
+
+/*
+Task 1: Create a class RightTriangle.java and print it (do not use println())
+          *
+          * *
+          * * *
+          * * * *
+          * * * * *
+ */
