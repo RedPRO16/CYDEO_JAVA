061369e65b1d6f00be5fa1a3ac071f27be8c3022
Nesho RADEV <40929562+RedPRO16@users.noreply.github.com>
2023-03-08 23:28:57 +0000

    latest update

--- JavaProgramming/src/day03_EscapeSequences/triangle_2.java
+++ JavaProgramming/src/day03_EscapeSequences/triangle_2.java
@@ -4,8 +4,24 @@ public class triangle_2 {
     public static void main(String[] args) {
 
         //Escape sequence
-        // \
-        System.out.println("/n");
+        // \n append a new line
+        System.out.print("Adam\n");
+        System.out.print("Eyup");
+
+        System.out.println();
+        // \t paragraph space(tab)
+
+        System.out.print("Adam\tEyup\n");
+        // \\ single bf slash
+
+        System.out.println("Computer\\Laptop");
+        // \" I want to add " in my string
+
+        //Car("Mercedes")
+        System.out.println("Car(\"Mercedes\")");
+
+        System.out.print("*\n* *\n* * *\n* * * *\n* * * * *");
+
     }
 }
 
