843f549806f0aeeb546d470c8831abc09f88a231
Nesho RADEV <40929562+RedPRO16@users.noreply.github.com>
2023-03-09 16:39:02 +0000

    day07

--- JavaPractice_Task/src/day07_Operators/ArithmeticOperators.java
+++ JavaPractice_Task/src/day07_Operators/ArithmeticOperators.java
@@ -6,7 +6,7 @@ public class ArithmeticOperators {
         int num1 = 10;
         int num2 = 3;
 
-        int division = num2 / num2; //division variable contains the result of num1 divided by num2
+        int division = num1 / num2; //division variable contains the result of num1 divided by num2
         int remainder = num1 % num2; //remainder variable contains the remainder of num1 divided by num2
 
         //10 divide by 3 is equal to 3 with a remainder of 1.
