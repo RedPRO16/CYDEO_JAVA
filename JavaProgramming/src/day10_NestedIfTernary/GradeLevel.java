994cb9e0a6deab492367a412a26edc9642ca86a8
Nesho RADEV <40929562+RedPRO16@users.noreply.github.com>
2023-03-13 21:34:21 +0000

    Day 10 tasks

---
+++ JavaProgramming/src/day10_NestedIfTernary/GradeLevel.java
@@ -0,0 +1,42 @@
+package day10_NestedIfTernary;
+
+import java.sql.PreparedStatement;
+
+public class GradeLevel {
+    public static void main(String[] args) {
+        byte grade = 10;
+        String result = "";
+
+        if (grade >= 1 && grade <= 18) {
+            if (grade <= 5) {
+                result = "Elementery school";
+            } else if (grade <= 8) {
+                result = "Middle school";
+            } else if (grade <= 12) {
+                result = "High school";
+            } else if (grade <= 16) {
+                result = "College";
+            } else {
+                result = "Grand school";
+            }
+        } else {
+            result = "Invalid grade";
+        }
+        System.out.println("result = " + result);
+    }
+}
+
+
+/*
+ Create a class called GradeLevel, Given a number(byte) grade level determine and print which school type someone is in.
+            grade level and types are:
+                    1-5: Elementary school
+                    6-8: Middle school
+                    9-12: High school
+                    13-16: College
+                    17-18: Grad School
+
+                    For Any Other grade: Invalid grade level given
+
+            NOTE: MUST USE NESTED IF. DO NOT USE MORE THAN ONE PRINT STATEMENT
+ */
